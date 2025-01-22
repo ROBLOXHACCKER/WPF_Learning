3e116b13f121a52d4ad4747da904f101e94d2b1c
ROBLOXHACCKER <emailcreatasulmomento@gmail.com>
2025-01-22 13:17:28 +0100

    prova

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void switch_bool(ref bool toggle)
+        {
+            if (toggle)
+            {
+                toggle = false;
+            }
+            else
+            {
+                toggle = true;
+            }
+        }
+
+        private void btn_toggleRun(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                tb_status.Text = tb_status.Text + " BRUTTISSIMA";
+            }else if (!running)
+            {
+                tb_status.Text = "ELISA BRUTTA";
+            }
+            switch_bool(ref running);
+        }
+    }
+}
